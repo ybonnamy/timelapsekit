d04f0c9d1f494e0bb634e10dc57e8778d7c77026
Yann BONNAMY <yann_bonnamy@yahoo.fr>
2020-04-12 14:44:26 +0200

    avoid slow copy

--- var/motion/generate.sh
+++ var/motion/generate.sh
@@ -2,6 +2,6 @@
 mencoder mf:////var/motion/*IntegratedWebcam.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/IntegratedWebcam.tmp.avi
 mencoder mf:////var/motion/*pwc.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/pwc.tmp.avi
 mencoder mf:////var/motion/*EndoscopeUSB.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/EndoscopeUSB.tmp.avi
-cp /var/motion/timelapse/IntegratedWebcam.tmp.avi /var/motion/timelapse/IntegratedWebcam.avi
-cp /var/motion/timelapse/pwc.tmp.avi /var/motion/timelapse/pwc.avi
-cp /var/motion/timelapse/EndoscopeUSB.tmp.avi /var/motion/timelapse/EndoscopeUSB.avi
+mv /var/motion/timelapse/IntegratedWebcam.tmp.avi /var/motion/timelapse/IntegratedWebcam.avi
+mv /var/motion/timelapse/pwc.tmp.avi /var/motion/timelapse/pwc.avi
+mv /var/motion/timelapse/EndoscopeUSB.tmp.avi /var/motion/timelapse/EndoscopeUSB.avi
