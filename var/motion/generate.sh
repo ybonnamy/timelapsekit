86c9774e2cc6b9d748a0ebfa51c41173db18de16
Yann BONNAMY <yann_bonnamy@yahoo.fr>
2020-04-12 14:30:08 +0200

    avoid issues

--- var/motion/generate.sh
+++ var/motion/generate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mencoder mf:////var/motion/*IntegratedWebcam.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/IntegratedWebcam.avi
-mencoder mf:////var/motion/*pwc.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/pwc.avi
-mencoder mf:////var/motion/*EndoscopeUSB.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/EndoscopeUSB.avi
+mencoder mf:////var/motion/*IntegratedWebcam.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/IntegratedWebcam.tmp.avi
+mencoder mf:////var/motion/*pwc.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/pwc.tmp.avi
+mencoder mf:////var/motion/*EndoscopeUSB.jpg -mf w=640:h=480:fps=25:type=jpg -ovc copy -oac copy -o /var/motion/timelapse/EndoscopeUSB.tmp.avi
+cp /var/motion/timelapse/IntegratedWebcam.tmp.avi /var/motion/timelapse/IntegratedWebcam.avi
+cp /var/motion/timelapse/pwc.tmp.avi /var/motion/timelapse/pwc.avi
+cp /var/motion/timelapse/EndoscopeUSB.tmp.avi /var/motion/timelapse/EndoscopeUSB.avi
